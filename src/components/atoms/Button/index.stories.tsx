3073a6f3b47a7495920b0d216a5a66a08efb3dfd
makotia <3mdevmail@gmail.com>
2021-09-03 19:25:33 +0900

    add stories

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Component, { Props } from '.'
+
+export default {
+  title: 'components/atoms/Button',
+  component: Component,
+  argTypes: {
+    colorTheme: {
+      options: ['dark', 'light'],
+      control: { type: 'select' },
+      defaultValue: 'dark',
+    }
+  }
+} as Meta
+
+const Template: Story<Props> = args => <Component {...args} />
+
+export const Dark = Template.bind({})
+Dark.args = {
+  buttonText: 'ハムストリングス',
+  colorTheme: 'dark',
+  linkTo: 'https://kaiseihp.jp/news/id_11936',
+  border: false,
+}
+
+export const Light = Template.bind({})
+Light.args = {
+  buttonText: 'テイクオーバーゾーン',
+  colorTheme: 'light',
+  linkTo: 'https://kotobank.jp/word/%E3%83%86%E3%83%BC%E3%82%AF%E3%82%AA%E3%83%BC%E3%83%90%E3%83%BC%E3%82%BE%E3%83%BC%E3%83%B3-672484',
+  border: false,
+}
---
+++ src/components/molecules/ButtonList/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Component from '.'
+
+type Props = {}
+
+export default {
+  title: 'components/molecules/ButtonList',
+  component: Component,
+} as Meta
+
+export const Template: Story<Props> = _ => <Component />
